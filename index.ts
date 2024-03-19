ae812fbc25a4637167fd4d395879c2920388faa3
TayyabAhmed890 <160255633+TayyabAhmed890@users.noreply.github.com>
2024-03-19 21:52:23 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,84 @@
+// Types of Objects
+// There are Two Methods of assign datatype in Objects..!
+// 1) interface  ==> Use in Only Array and Objects
+// 2) Type ==> Use in Other Common Datatypes..!
+
+type empType = {
+    Name : string;
+    ID : number;
+    Department :string;
+    Designation :string;
+    BankDetails : {
+        AccountNumber :number;
+        AccountType :string;
+        Salary:string;
+        isActive:boolean;
+    
+    }
+    
+}
+
+interface personalDetails {
+    Age : number;
+    IQ : number;
+    Typspeed : string;
+    Hobbies :string[];
+}
+
+
+let myEmployee:empType = {
+    Name :"Tayyab Ahmed",
+    ID :108091,
+    Department :"Information Technology",
+    Designation :"SR.Developer",
+    BankDetails : {
+        AccountNumber :12346789045, 
+        AccountType :"Current", 
+        Salary:"45000 PKR",
+        isActive:true,
+    }
+}
+
+let empDetails:personalDetails = {
+
+    Age : 20,
+    IQ : 79,
+    Typspeed : "25 WPM",
+    Hobbies :["Gaming","Coding","Cricket","Writting"],
+
+}
+
+let myEmployee1:empType = {
+    Name :"Ali Ahmed",
+    ID :108092,
+    Department :"Information Technology",
+    Designation :"JR.Developer",
+    BankDetails : {
+        AccountNumber :128946789045, 
+        AccountType :"Current", 
+        Salary:"35000 PKR",
+        isActive:true,
+    }
+}
+
+let empDetails1:personalDetails = {
+
+    Age : 19,
+    IQ : 81,
+    Typspeed : "29 WPM",
+    Hobbies :["Gaming","Coding","Cricket","Reading"],
+
+}
+
+console.log(myEmployee);
+console.log(empDetails);
+
+console.log(myEmployee1);
+console.log(empDetails1);
+
+console.log(empDetails1.IQ);
+console.log("The Employee ID is: ",myEmployee.ID);
+
+
+
+
